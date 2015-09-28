93512d3c525feee1b84442da4d7d7d8bd7236123
andreacorzani <andrea.corzani@spot.it>
2015-09-28 08:54:12 +0200

    to fix.

--- client/app/blocks/logger_vts/logger.service.js
+++ client/app/blocks/logger_vts/logger.service.js
@@ -4,9 +4,29 @@ var Blocks;
     (function (Logger) {
         var LoggerService = (function () {
             function LoggerService($log, toastr) {
-                this.log = $log;
+                this.$log = $log;
                 this.toastr = toastr;
             }
+            LoggerService.prototype.logInfo = function (message, data, title) {
+                this.toastr.info(message, title);
+                this.$log.info('Info: ' + message, data);
+            };
+            LoggerService.prototype.logError = function (message, data, title) {
+                this.toastr.error(message, title);
+                this.$log.error('Error: ' + message, data);
+            };
+            LoggerService.prototype.logSuccess = function (message, data, title) {
+                this.toastr.success(message, title);
+                this.$log.info('Success: ' + message, data);
+            };
+            LoggerService.prototype.logWarning = function (message, data, title) {
+                this.toastr.warning(message, title);
+                this.$log.warn('Warning: ' + message, data);
+            };
+            LoggerService.prototype.log = function (message, data, title) {
+                this.toastr.error(message, title);
+                this.$log.error('Error: ' + message, data);
+            };
             return LoggerService;
         })();
         angular.module('blocks.logger').factory('logger', LoggerService);
--- client/app/blocks/logger_vts/logger.service.ts
+++ client/app/blocks/logger_vts/logger.service.ts
@@ -14,37 +14,38 @@ module Blocks.Logger {
     
     class LoggerService {
 
-        log: ng.ILogService;
+        $log: ng.ILogService;
+        
         toastr: any;
 
         constructor($log: ng.ILogService, toastr: any) {
-            this.log = $log;
+            this.$log = $log;
             this.toastr = toastr;
         }
 
-        function logInfo(message: string, data: Object, title: string) {
+        logInfo(message: string, data: Object, title: string) {
             this.toastr.info(message, title);
-            this.log.info('Info: ' + message, data);
+            this.$log.info('Info: ' + message, data);
         }
     
-        function logError(message: string, data: Object, title: string) {
+        logError(message: string, data: Object, title: string) {
             this.toastr.error(message, title);
-            this.log.error('Error: ' + message, data);
+            this.$log.error('Error: ' + message, data);
         }
     
-        function logSuccess(message: string, data: Object, title: string) {
+        logSuccess(message: string, data: Object, title: string) {
             this.toastr.success(message, title);
-            this.log.info('Success: ' + message, data);
+            this.$log.info('Success: ' + message, data);
         }
     
-        function logWarning(message: string, data: Object, title: string) {
+        logWarning(message: string, data: Object, title: string) {
             this.toastr.warning(message, title);
-            this.log.warn('Warning: ' + message, data);
+            this.$log.warn('Warning: ' + message, data);
         }
     
-        function log(message: string, data: Object, title: string) {
+        log(message: string, data: Object, title: string) {
             this.toastr.error(message, title);
-            this.log.error('Error: ' + message, data);
+            this.$log.error('Error: ' + message, data);
         }
         
         /*var service : ILoggerService = {
